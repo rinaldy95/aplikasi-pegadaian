5b64bc47191f60b9e61eb65389600ec89a3d2f1e
rinaldy95 <rinaldymarasabessy@gmail.com>
2016-03-15 13:24:28 +0800

    server

---
+++ pegadaian/src/main/java/com/pegadaian/AplikasiPedagaianApplication.java
@@ -0,0 +1,12 @@
+package com.pegadaian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplikasiPedagaianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplikasiPedagaianApplication.class, args);
+	}
+}
---
+++ pegadaian/src/test/java/com/pegadaian/AplikasiPedagaianApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pegadaian;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = AplikasiPedagaianApplication.class)
+public class AplikasiPedagaianApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
